ff5c0e3f76de4221278450abcccf3fc3f7a3da64
Madan Chaudhary <madan712@gmail.com>
2022-05-19 11:15:41 +0100

    HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './component/app';
 import appStore from './service/app-store';
@@ -9,4 +9,4 @@ import appStore from './service/app-store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 
-ReactDOM.render(<Provider store={appStore}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('app'));
+ReactDOM.render(<Provider store={appStore}><HashRouter><App /></HashRouter></Provider>, document.getElementById('app'));
